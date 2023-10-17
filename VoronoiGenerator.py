0aa6c15ada2b0c9d670b4fcfa327154889d559f6
Michael Ide <mide122006@gmail.com>
2023-10-17 09:07:23 -0700

    Update VoronoiGenerator.py (source: VoronoiTest2_7.py)

--- VoronoiGenerator.py
+++ VoronoiGenerator.py
@@ -7,10 +7,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 #import math
 
+#points = [(50, 50), (25, 25), (75, 75), (98, 70)]
 points = [(50, 50), (25, 25), (75, 75)]
 data = []
 #cell = []
 cell = {}
+intersection3Points = {}
 
 def distance(x1, y1, x2, y2):
 	return (((x1 - x2) ** 2) + ((y1 - y2) ** 2)) ** 0.5
@@ -105,9 +107,9 @@ for i in range(n):
 
 for i in range(n):
 	#cell.update({ f"{str(points[i]).replace(', ', '_')}" : {"otherPoint":[], "slope":None, "midpoint":[None, None], "boundA":[0, 200], "boundB":[200, 0]} })
-	cell.update({ f"{str(points[i]).replace(', ', '_')}" : {"otherPoint":[{ "(None_None)" : {"slope":None, "midpoint":[None, None], "boundA":[0, 200], "boundB":[200, 0]} }] } })
+	cell.update({ f"{str(points[i]).replace(', ', '_')}" : {"point":points[i], "otherPoint":{ "(None_None)" : {"point":[None, None], "slope":None, "midpoint":[None, None], "boundA":[0, 200], "boundB":[200, 0]} } } })
 
-print(cell)
+#print(cell)
 
 # Go through the points and find all the edges with the nearest point (will need to change this later as most cells have edges with more than just their nearest neighbor)
 for pt in points:
@@ -195,21 +197,23 @@ for pt in points:
 		#print(cell[f"({a}_{b})"])
 		#print(list(cell[f"({a}_{b})"]["otherPoint"][0].keys())[0])
 		#if the entry already exists, don't change it (do nothing) (?)
-		if "(None_None)" == list(cell[f"({a}_{b})"]["otherPoint"][0].keys())[0]:
-			print("b")
-			cell.update({ f"({a}_{b})" : {"otherPoint":[{ f"({c}_{d})" : {"slope":-1/m, "midpoint":[mPtX, mPtY], "boundA":[0, (-1/m) * (-1 * mPtX) + mPtY], "boundB":[mPtX - (mPtY/ (-1/m)), 0]} }] } })
+
+		if "(None_None)" == list(cell[f"({a}_{b})"]["otherPoint"].keys())[0]:
+			#print("a")
+			cell.update({ f"({a}_{b})" : {"point":[a, b], "otherPoint":{ f"({c}_{d})" : {"point":[c, d], "slope":-1/m, "midpoint":[mPtX, mPtY], "boundA":[0, (-1/m) * (-1 * mPtX) + mPtY], "boundB":[mPtX - (mPtY/ (-1/m)), 0]} } } })
 			#del cell[f"({a}_{b})"]["otherPoint"][0]
 		elif not f"({a}_{b})" in cell:
 			#cell[f"({a}_{b})"] = {}
-			print("c")
-			cell.update({ f"({a}_{b})" : {"otherPoint":[{ f"({c}_{d})" : {"slope":-1/m, "midpoint":[mPtX, mPtY], "boundA":[0, (-1/m) * (-1 * mPtX) + mPtY], "boundB":[mPtX - (mPtY/ (-1/m)), 0]} }] } })
+			#print("b")
+			cell.update({ f"({a}_{b})" : {"point":[a, b], "otherPoint":{ f"({c}_{d})" : {"point":[c, d], "slope":-1/m, "midpoint":[mPtX, mPtY], "boundA":[0, (-1/m) * (-1 * mPtX) + mPtY], "boundB":[mPtX - (mPtY/ (-1/m)), 0]} } } })
 		elif f"({a}_{b})" in cell:
-			print("d")
-			try:
-				cell[f"({a}_{b})"]["otherPoint"].index({ f"({c}_{d})" : {"slope":-1/m, "midpoint":[mPtX, mPtY], "boundA":[0, (-1/m) * (-1 * mPtX) + mPtY], "boundB":[mPtX - (mPtY/ (-1/m)), 0]} })
-			except Exception:
-				print("e")
-				cell[f"({a}_{b})"]["otherPoint"].append({ f"({c}_{d})" : {"slope":-1/m, "midpoint":[mPtX, mPtY], "boundA":[0, (-1/m) * (-1 * mPtX) + mPtY], "boundB":[mPtX - (mPtY/ (-1/m)), 0]} } )
+			if not f"({c}_{d})" in cell[f"({a}_{b})"]["otherPoint"]:
+				#print("c")
+				cell[f"({a}_{b})"]["otherPoint"].update({ f"({c}_{d})" : {"point":[c, d], "slope":-1/m, "midpoint":[mPtX, mPtY], "boundA":[0, (-1/m) * (-1 * mPtX) + mPtY], "boundB":[mPtX - (mPtY/ (-1/m)), 0]} } )
+			#print("d")
+			
+		#print(cell[f"({a}_{b})"]["otherPoint"])
+		#print(f"({a}_{b}) ({c}_{d})")
 		
 		#if f"({a}_{b})" in cell:
 			#if the entry already already exists
@@ -230,9 +234,16 @@ for pt in points:
 
 #print(data.__len__())
 
+#print(cell["(98_70)"]["otherPoint"])
+#print(cell["(25_25)"]["otherPoint"])
 print(cell)
 
 # Go through the points and find the intersection points in order to form edges
+#-------------------------------------------------------------------------------------------------Current issues: 
+#1) a perpendicular line between two of the points is now showing up
+#2) no intersection points are being found where there should be at least 1 (?)
+#3) no triple intersection points are being found whne there should be 1
+
 for currentPoint in points:
 	#points2 = points.copy()
 	#data2 = data.copy()
@@ -273,30 +284,97 @@ for currentPoint in points:
 #				data2[i]["boundB"][1] = y
 
 	cell2 = cell.copy()
-	
-	kys = cell2.keys()
+	arrayPoint = str(currentPoint).replace(", ", "_")
+
+	#kys = cell2.keys()
 	
 	def sort(target, array):
 		n = len(array)
-	
+
+		keys = list(array.keys())
+
 		for i in range(n):
 			for j in range(0, n - i - 1):
-				point1 = array[j]["midpoint"]
-				point2 = array[j + 1]["midpoint"]
+				point1 = array[keys[j]]["midpoint"]
+				point2 = array[keys[j + 1]]["midpoint"]
 
 				distance1 = distance(target[0], target[1], point1[0], point1[1])
 				distance2 = distance(target[0], target[1], point2[0], point2[1])
 
 				if distance1 > distance2:
-					array[j], array[j + 1] = array[j + 1], array[j]
+					array[keys[j]], array[keys[j + 1]] = array[keys[j + 1]], array[keys[j]]
 
-	for key in kys:
-		#need to sort the set of otherPoints based on the distance to the point, but first need to decide if I want otherPoint to be a dictionary or a list because a list of dictionary items is kind of the worst of both worlds
-		#update: a dictionary would probably be better since a dictionary can be iterated though as easily as a list using the .keys() and can check if an item exists without causing an error
-		cell2[key]
+	#The easiest way to get the verticies of the edges may be to just find all of the interesection points of 3 lines and keep track of the points that made them,
+	#	then connect each of those verticies to 3 other verticies (stopping at this step would result in no edges being connected to the boundry, which is an issue)
+	#	The above issue could be solved by including finding intersections with the boundaries after finding all the other intersections and only requiring 2 lines
+	#		(the line and the boundary) to interesect in this case
+
+	#for key in kys:
+
+		#print(list(map(int, key.replace("_", ", ").removesuffix(")").removeprefix("(").split(", "))))
+	#	sort(list(map(int, key.replace("_", ", ").removesuffix(")").removeprefix("(").split(", "))), cell2[key]["otherPoint"])
+		#print(cell2)
+
+	#print(cell2[arrayPoint])
+
+	sort(cell2[arrayPoint]["point"], cell2[arrayPoint]["otherPoint"])
+
+	kys = list(cell2[arrayPoint]["otherPoint"].keys())
+
+	#n = kys.__len__()
+	print("a")
+	for key1 in kys:
+		point1 = cell2[arrayPoint]["otherPoint"][key1]
+		print(" b")
+		for key2 in kys:
+			print("  c")
+			if key2 != key1:
+				print("   d")
+				point2 = cell2[arrayPoint]["otherPoint"][key2]
+
+				intX, intY = intersectSolver(point1, point2)
+
+				if intX != None:
+					print("     e")
+					for key3 in kys:
+						print("      f")
+						if key3 != key1 and key3 != key2:
+							print("       g")
+							point3 = cell2[arrayPoint]["otherPoint"][key3]
+							
+							y = equationToPoint(point3, intX)
+
+							if y == intY:
+								print("        h")
+								if intX > point1["midpoint"][0]:
+									point1["boundB"] = [intX, intY]
+								#elif intX < point1["midpoint"][0]:
+								else:
+									point1["boundA"] = [intX, intY]
+
+								if intX > point2["midpoint"][0]:
+									point2["boundB"] = [intX, intY]
+								#elif intX < point2["midpoint"][0]:
+								else:
+									point2["boundA"] = [intX, intY]
+
+								if intX > point3["midpoint"][0]:
+									point3["boundB"] = [intX, intY]
+								#elif intX < point3["midpoint"][0]:
+								else:
+									point3["boundA"] = [intX, intY]
+
+								intersection3Points.update({
+"intPosition":[intX, intY], 
+"line1":{"point":point1["point"], "slope":point1["slope"], "midpoint":point1["midpoint"], "boundA":point1["boundA"], "boundB":point1["boundB"]},
+"line2":{"point":point2["point"], "slope":point2["slope"], "midpoint":point2["midpoint"], "boundA":point2["boundA"], "boundB":point2["boundB"]},
+"line3":{"point":point3["point"], "slope":point3["slope"], "midpoint":point3["midpoint"], "boundA":point3["boundA"], "boundB":point3["boundB"]}
+})
+
+print(intersection3Points)
 
 # Plot the data
-for info in data:
+#for info in data:
 	#midPt = info["midpoint"]
 	
 	#y1 = equationToPoint(info, info["boundA"][0])
@@ -305,14 +383,24 @@ for info in data:
 	#plt.plot([info["boundA"][0], info["boundB"][0]], [y1, y2], "-go")
 	#print(info["boundA"][0])
 	#print(f'{info["boundA"][0], info["boundA"][1]} {info["boundB"][0], info["boundB"][1]}')
-	plt.plot([info["boundA"][0], info["boundB"][0]], [info["boundA"][1], info["boundB"][1]], "-go")
+#	plt.plot([info["boundA"][0], info["boundB"][0]], [info["boundA"][1], info["boundB"][1]], "-go")
 
-	plt.plot(info["midpoint"][0], info["midpoint"][1], "yo")
+#	plt.plot(info["midpoint"][0], info["midpoint"][1], "yo")
+print(cell.__len__())
+for site in cell:
+	plt.plot(cell[site]["point"][0], cell[site]["point"][1], "ro")
 
-for pt in points:
-	plt.plot(pt[0], pt[1], "ro")
+	for line in cell[site]["otherPoint"]:
+		
+		plt.plot(cell[site]["otherPoint"][line]["midpoint"][0], cell[site]["otherPoint"][line]["midpoint"][1], "yo")
+
+		plt.plot([cell[site]["otherPoint"][line]["boundA"][0], cell[site]["otherPoint"][line]["boundB"][0]], [cell[site]["otherPoint"][line]["boundA"][1], cell[site]["otherPoint"][line]["boundB"][1]], "-go")
+
+#for pt in points:
+#	plt.plot(pt[0], pt[1], "ro")
 
 plt.show()
+
 
 
 
